9ea7f138d1b665709de126a860f50b73c1a1b7a6
Elbaraa-Saber <baraax01@gmail.com>
2026-05-17 19:14:08 +0700

    Fix SRP violation in Program using Extract Class

---
+++ src/LifeSim/ConsoleWorldRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace LifeSim;
+
+public sealed class ConsoleWorldRenderer
+{
+    public void Render(World world)
+    {
+        Console.SetCursorPosition(0, 0);
+
+        var plants = world.All.OfType<Plant>().Count();
+        var herbivores = world.All.OfType<Herbivore>().Count();
+        var predators = world.All.OfType<Predator>().Count();
+
+        Console.ResetColor();
+        Console.WriteLine($"Tick: {world.Tick,-8}  Plants: {plants,-5}  Herbivores: {herbivores,-5}  Predators: {predators,-5}   [Space/P] pause, [Q/Esc] quit");
+
+        var snapshot = world.GridSnapshot();
+        for (var y = 0; y < world.Height; y++)
+        {
+            for (var x = 0; x < world.Width; x++)
+            {
+                if (snapshot.TryGetValue(new Point2(x, y), out var organism))
+                {
+                    organism.ApplyColor();
+                    Console.Write(organism.Glyph);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- src/LifeSim/Program.cs
+++ src/LifeSim/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading;
 
 namespace LifeSim;
@@ -21,6 +20,8 @@ public static class Program
         world.Seed<Plant>(initialPlants);
         world.Seed<Herbivore>(initialHerbivores);
         world.Seed<Predator>(initialPredators);
+
+        var renderer = new ConsoleWorldRenderer();
 
         var paused = false;
         const int delayMs = 120;
@@ -46,42 +47,10 @@ public static class Program
             if (!paused)
             {
                 world.Step();
-                RenderWorld(world);
+                renderer.Render(world);
             }
 
             Thread.Sleep(delayMs);
-        }
-    }
-
-    private static void RenderWorld(World world)
-    {
-        Console.SetCursorPosition(0, 0);
-
-        var plants = world.All.OfType<Plant>().Count();
-        var herbs = world.All.OfType<Herbivore>().Count();
-        var preds = world.All.OfType<Predator>().Count();
-
-        Console.ResetColor();
-        Console.WriteLine($"Tick: {world.Tick,-8}  Plants: {plants,-5}  Herbivores: {herbs,-5}  Predators: {preds,-5}   [Space/P] pause, [Q/Esc] quit");
-
-        var snapshot = world.GridSnapshot();
-        for (var y = 0; y < world.Height; y++)
-        {
-            for (var x = 0; x < world.Width; x++)
-            {
-                if (snapshot.TryGetValue(new Point2(x, y), out var organism))
-                {
-                    organism.ApplyColor();
-                    Console.Write(organism.Glyph);
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Console.Write(' ');
-                }
-            }
-
-            Console.WriteLine();
         }
     }
 }
