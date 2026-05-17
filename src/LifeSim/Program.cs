82ac65be8543202b076dff33df8afd9e1039e6fd
Elbaraa-Saber <baraax01@gmail.com>
2026-05-17 19:24:57 +0700

    Refactor Program.Main long method using Extract Class

--- src/LifeSim/Program.cs
+++ src/LifeSim/Program.cs
@@ -1,56 +1,10 @@
-using System;
-using System.Threading;
-
 namespace LifeSim;
 
 public static class Program
 {
     public static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.CursorVisible = false;
-
-        const int width = 50;
-        const int height = 22;
-        var initialPlants = (int)(width * height * 0.22);
-        const int initialHerbivores = 28;
-        const int initialPredators = 10;
-
-        var world = new World(width, height);
-        world.Seed<Plant>(initialPlants);
-        world.Seed<Herbivore>(initialHerbivores);
-        world.Seed<Predator>(initialPredators);
-
-        var renderer = new ConsoleWorldRenderer();
-
-        var paused = false;
-        const int delayMs = 120;
-
-        while (true)
-        {
-            while (!Console.IsInputRedirected && Console.KeyAvailable)
-            {
-                var key = Console.ReadKey(true).Key;
-                if (key == ConsoleKey.Q || key == ConsoleKey.Escape)
-                {
-                    Console.ResetColor();
-                    Console.CursorVisible = true;
-                    return;
-                }
-
-                if (key == ConsoleKey.Spacebar || key == ConsoleKey.P)
-                {
-                    paused = !paused;
-                }
-            }
-
-            if (!paused)
-            {
-                world.Step();
-                renderer.Render(world);
-            }
-
-            Thread.Sleep(delayMs);
-        }
+        var simulationRunner = new SimulationRunner();
+        simulationRunner.Run();
     }
 }
